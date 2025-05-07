25686e1281794ce90db115a1f7eda5b556ecaa21
Lucas <lucasarruas@gmail.com>
2025-05-06 22:36:57 -0300

    Commited

---
+++ src/test/java/automatizado/pageObject/BasePO.java
@@ -0,0 +1,8 @@
+package automatizado.pageObject;
+
+/**
+ * Classe base para criação das novas PageObjects.
+ * Todas as pages devem ser herdadas dessa classe.
+ */
+public abstract class BasePO {
+}
--- src/test/java/automatizado/teste/GoogleTest.java
+++ src/test/java/automatizado/teste/GoogleTest.java
@@ -1,37 +1,34 @@
 package automatizado.teste;
 
+import automatizado.page.GooglePO;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertTrue;
 
-public class GoogleTest {
-    private WebDriver driver;
-    private final  String URL_BASE = "https://www.google.com/";
-    private  final String CAMINHO_DRIVER = "src/test/java/automatizado/resource/chromedriver -v 136.0.7103.93.exe";
+public class GoogleTest extends BaseTest{
 
-    private void iniciar(){
-        System.setProperty("webdriver.chrome.driver",CAMINHO_DRIVER);
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(URL_BASE);
+    private  static GooglePO googlePage;
+    @BeforeClass
+    public  static void prepararTestes(){
+        googlePage = new GooglePO(driver);
 
     }
+
     @Test
-    public void devePesquisarNoGoogle(){
-        iniciar();
+    public void TC001_deveSerPossivelPesquisarNoGoogleOTexto(){
+        googlePage.pesquisar("O senhor dos anéis");
 
-        WebElement inputPesquisa = driver.findElement(By.name("q"));
-        inputPesquisa.sendKeys("Batata Frita"+ Keys.ENTER);
-
-        String resultado = driver.findElement(By.id("result-stats")).getText();
+        String resultado = googlePage.obterResultadoDaPesquisa();
         assertTrue(resultado, resultado.contains("Aproximadamente"));
+    }
 
-        driver.quit();
+    @Test
+    public void TC002_deveSerPossivelPesquisarNoGoogleOTexto(){
+        googlePage.pesquisar("O Fantasma da Opera");
+
+        String resultado = googlePage.obterResultadoDaPesquisa();
+        assertTrue(resultado, resultado.contains("resultados"));
     }
 
 }
